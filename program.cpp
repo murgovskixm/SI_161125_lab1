74661a7673ceabcc0dea1e8c6c68288d5507199c
unknown <murgovskixm@gmail.com>
2019-04-16 16:16:51 +0200

    main part implemented

--- program.cpp
+++ program.cpp
@@ -4,12 +4,15 @@ using namespace std;
 
 void printMyName () {
 	//TODO
+cout<<"Martin Murgovski"<<endl;
 }
 
 void printMyIndex () {
 	//TODO
+cout<<"161125"<<endl;
 }
 
 int main () {
-	//TODO
+	printMyName();
+	printMyIndex();
 }
